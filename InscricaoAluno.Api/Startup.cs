297958d614a51ffce569f168fb204c79b7a53520
Rui Santos <rui@digiexpress.com.br>
2018-11-27 12:01:43 -0200

    Inclusão do frontend

--- InscricaoAluno.Api/Startup.cs
+++ InscricaoAluno.Api/Startup.cs
@@ -25,11 +25,6 @@ namespace InscricaoAluno.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            services.AddDbContext<InscricaoAlunoApiContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("InscricaoAlunoApiContext")));
-
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info
@@ -58,6 +53,20 @@ namespace InscricaoAluno.Api
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
             });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<InscricaoAlunoApiContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("InscricaoAlunoApiContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -76,6 +85,8 @@ namespace InscricaoAluno.Api
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Inscrição de Aluno");
             });
+
+            app.UseCors("CorsPolicy");
 
             app.UseMvc();
         }
